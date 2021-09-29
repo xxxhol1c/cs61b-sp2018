7e2d74d8fd12d3af4ea2ddca585d9426303f1877
xxxhol1c <hol1cMin@icloud.com>
2021-09-30 01:40:34 +0800

    completed lab1

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -1,7 +1,19 @@
 /** Class that determines whether or not a year is a leap year.
- *  @author YOUR NAME HERE
+ *  @author xxxhol1c
  */
 public class LeapYear {
+    /** Check whether or not a year is a leap year.
+     *  @param year to be checked
+    */
+    public static boolean isLeapYear(int year) {
+        if (year % 400 == 0) {
+            return true;
+        } else if (year % 4 == 0 && year % 100 != 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 
     /** Calls isLeapYear to print correct statement.
      *  @param  year to be analyzed
@@ -30,4 +42,3 @@ public class LeapYear {
         }
     }
 }
-
