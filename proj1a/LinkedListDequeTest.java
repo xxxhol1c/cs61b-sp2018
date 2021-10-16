e2c7e9667b8905129fb1356386418ca192d145c2
xxxhol1c <hol1cMin@icloud.com>
2021-10-17 02:45:22 +0800

    completed proj1a

--- proj1a/LinkedListDequeTest.java
+++ proj1a/LinkedListDequeTest.java
@@ -28,8 +28,8 @@ public class LinkedListDequeTest {
     }
 
 
-    /** Adds a few things to
-     * the list, checking isEmpty() and size() are correct,
+    /** Adds a few things to the list,
+     * checking isEmpty() and size() are correct,
      * finally printing the results.
      * && is the "and" operation. */
     public static  void addIsEmptySizeTest() {
