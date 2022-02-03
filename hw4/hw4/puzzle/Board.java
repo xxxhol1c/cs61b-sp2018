ca3f8b31097b19d7896067cbea973817d4d94a7d
xxxhol1c <hol1cmin@icloud.com>
2022-02-04 01:00:46 +0800

    completed hw4

--- hw4/hw4/puzzle/Board.java
+++ hw4/hw4/puzzle/Board.java
@@ -1,10 +1,138 @@
 package hw4.puzzle;
 
-public class Board {
+import edu.princeton.cs.algs4.Queue;
+
+public class Board implements WorldState{
+    private final int[][] tiles;
+    private static final int BLANK = 0;
+    private int N;
+    private int estimatedDist;
+
+
+    public Board(int[][] tiles) {
+        N = tiles.length;
+        this.tiles = new int[N][N];
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                this.tiles[i][j] = tiles[i][j];
+            }
+        }
+    }
+
+    public int tileAt(int i, int j) {
+        if (i < 0 || j < 0 || i >= N || j >= N) {
+            throw new IndexOutOfBoundsException();
+        }
+        return tiles[i][j];
+    }
+    public int size() {
+        return N;
+    }
+
+    /**
+     * Returns neighbors of this board.
+     * cited from http://joshh.ug/neighbors.html
+     */
+    @Override
+    public Iterable<WorldState> neighbors() {
+        Queue<WorldState> neighbors = new Queue<>();
+        int hug = size();
+        int bug = -1;
+        int zug = -1;
+        for (int rug = 0; rug < hug; rug++) {
+            for (int tug = 0; tug < hug; tug++) {
+                if (tileAt(rug, tug) == BLANK) {
+                    bug = rug;
+                    zug = tug;
+                }
+            }
+        }
+        int[][] ili1li1 = new int[hug][hug];
+        for (int pug = 0; pug < hug; pug++) {
+            for (int yug = 0; yug < hug; yug++) {
+                ili1li1[pug][yug] = tileAt(pug, yug);
+            }
+        }
+        for (int l11il = 0; l11il < hug; l11il++) {
+            for (int lil1il1 = 0; lil1il1 < hug; lil1il1++) {
+                if (Math.abs(-bug + l11il) + Math.abs(lil1il1 - zug) - 1 == 0) {
+                    ili1li1[bug][zug] = ili1li1[l11il][lil1il1];
+                    ili1li1[l11il][lil1il1] = BLANK;
+                    Board neighbor = new Board(ili1li1);
+                    neighbors.enqueue(neighbor);
+                    ili1li1[l11il][lil1il1] = ili1li1[bug][zug];
+                    ili1li1[bug][zug] = BLANK;
+                }
+            }
+        }
+        return neighbors;
+    }
+
+    public int hamming() {
+        int dist = 0;
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (tiles[i][j] == BLANK) {
+                    continue;
+                } else if (tiles[i][j] != i * N + j + 1) {
+                    dist++;
+                }
+            }
+        }
+        return dist;
+    }
+
+    private int intTOX(int n) {
+        return (n - 1) / N;
+    }
+
+    private int intTOY(int n) {
+        return (n - 1) % N ;
+    }
+
+    public int manhattan() {
+        int dist = 0;
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (tiles[i][j] ==  BLANK) {
+                    continue;
+                } else {
+                    dist += Math.abs(intTOX(tiles[i][j]) - i);
+                    dist += Math.abs(intTOY(tiles[i][j]) - j);
+                }
+            }
+        }
+        return dist;
+    }
+
+    public int estimatedDistanceToGoal() {
+        return manhattan();
+    }
+
+    @Override
+    public boolean equals(Object y) {
+        if (y == this) return true;
+        if (y == null || this.getClass() != y.getClass()) {
+            return false;
+        }
+        Board other = (Board) y;
+        if (this.N != other.N) return false;
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (this.tiles[i][j] != other.tiles[i][j]) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+
+
 
     /** Returns the string representation of the board. 
       * Uncomment this method. */
-    /*public String toString() {
+    public String toString() {
         StringBuilder s = new StringBuilder();
         int N = size();
         s.append(N + "\n");
@@ -16,6 +144,6 @@ public class Board {
         }
         s.append("\n");
         return s.toString();
-    }*/
+    }
 
 }
---
+++ hw4/hw4/puzzle/Solver.java
@@ -0,0 +1,63 @@
+package hw4.puzzle;
+
+import edu.princeton.cs.algs4.MinPQ;
+import edu.princeton.cs.algs4.Stack;
+
+public class Solver {
+
+    private class SearchNode implements Comparable<SearchNode> {
+        private WorldState ws;
+        private int move = 0;
+        private SearchNode pre;
+
+        private SearchNode(WorldState ws, int move, SearchNode pre) {
+            this.ws = ws;
+            this.move = move;
+            this.pre = pre;
+        }
+
+        @Override
+        public int compareTo(SearchNode o) {
+            return this.move + this.ws.estimatedDistanceToGoal() - o.move - o.ws.estimatedDistanceToGoal();
+        }
+    }
+
+    private MinPQ<SearchNode> nextNodes = new MinPQ<>();
+    private Stack<WorldState> bestNodes = new Stack<>();
+    private int enqueNum;
+
+    public Solver(WorldState initial) {
+        SearchNode curNode  = new SearchNode(initial, 0, null);
+        nextNodes.insert(curNode);
+        while(!nextNodes.isEmpty()) {
+            curNode = nextNodes.delMin();
+            if (curNode.ws.isGoal()) {
+                break;
+            }
+            for (WorldState neighbour : curNode.ws.neighbors()) {
+                if (curNode.pre == null || !neighbour.equals(curNode.pre.ws)) {
+                    nextNodes.insert(new SearchNode(neighbour, curNode.move + 1, curNode));
+                    enqueNum += 1;
+                }
+            }
+        }
+        for (SearchNode n = curNode; n != null; n = n.pre) {
+            bestNodes.push(n.ws);
+        }
+    }
+
+    public int moves() {
+        return bestNodes.size() - 1;
+    }
+
+    public Iterable<WorldState> solution() {
+        return bestNodes;
+    }
+
+    /**
+     * test for the critical optimization
+     */
+    public int test() {
+        return enqueNum;
+    }
+}
